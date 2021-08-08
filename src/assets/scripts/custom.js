b95d2dac4ba38ac832614182c46f80ce83bb980d
ValouZ <debray.va@gmail.com>
2021-08-09 01:04:31 +0200

    rotate marche vite fait

--- src/assets/scripts/custom.js
+++ src/assets/scripts/custom.js
@@ -1,8 +1,45 @@
 const imageResult = document.querySelector("#app-result");
 const validButton = document.querySelector("#app-valid");
+const rotate = document.querySelector("#app-rotate");
 
 imageResult.src = localStorage.image;
 
-validButton.addEventListener('click', () =>{
-  document.location.href = '/loading.html'
-})
+validButton.addEventListener("click", () => {
+  document.location.href = "/loading.html";
+});
+
+rotate.addEventListener("click", () => {
+  console.log("ROTATE");
+  let img = new Image();
+  img.src = imageResult.src;
+
+  // Create a canvas object.
+  let canvas = document.createElement("canvas");
+  // Wait till the image is loaded.
+  img.onload = function () {
+    rotateImage(img, canvas);
+    updateImage(canvas);
+  };
+});
+
+let rotateImage = function (img, canvas) {
+  console.log('rotateImage()')
+  // Create canvas context.
+  let ctx = canvas.getContext("2d");
+
+  // Assign width and height.
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+
+  // Rotate the image and draw it on the canvas.
+  ctx.rotate(Math.PI / 2);
+  ctx.drawImage(img, -img.width / 2, -img.height / 2);
+};
+
+let updateImage = function (canvas) {
+  console.log('updateImage()')
+  imageResult.src = canvas.toDataURL("image/png")
+  localStorage.image = imageResult.src
+};
